3c56818f5c497ea8e03ac10f38248382dc50bcfc
PR0C3S <50205406+PR0C3S@users.noreply.github.com>
2019-07-08 21:07:46 -0300

    Clase 2  / Monticulos

--- main.c
+++ main.c
@@ -1,63 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Pilaestatica.h"
+#include "Monticulo.h"
 
-void MostrarEncabezadoHistograma();
-void MostrarElementoHistograma(int, float, float);
-void MostrarPieHistograma(int, float);
-
-int main(){
-   int cantidad=0;
-   float nuevoValor, total=0;
-    Pila *miPila;
-    miPila= IniciarPila(miPila);
+int main()
+{
+    float nuevoValor;
+    Monticulo *miHeap;
+    miHeap = Inicializar(1);
 
     do
     {
-       printf("\nDigite un valor: (-1 para terminar): ");
-       scanf("%f",&nuevoValor);
-       if(nuevoValor >= 0)
-       {
-            total+=nuevoValor;
-          //  cantidad++;
-         Push(miPila,nuevoValor);
-       }
-    }while(nuevoValor >=0);
-    // o calcular la cantidad de esta forma cantidad= Size(miPila);
-    MostrarEncabezadoHistograma();
+        printf("Digite un valor (0 para salir): ");
+        scanf("%f", &nuevoValor);
 
-    while(!isEmpty(miPila))
+        if(nuevoValor != 0)
+            Insertar(miHeap, nuevoValor);
+    }while(nuevoValor != 0);
+    // 5, 10, 20, 35, 2, 8, 15, 100, 9, 1
+
+    while(!isEmpty(miHeap))
     {
-       cantidad++;
-       nuevoValor= Pop(miPila);
-       MostrarElementoHistograma(cantidad, nuevoValor,nuevoValor/total*100);
+        printf("%.2f\t", Poll(miHeap));
     }
 
-    MostrarPieHistograma(cantidad,total);
     return 0;
-}
-
-void MostrarEncabezadoHistograma()
-{
-   system("cls");
-   printf("HISTOGRAMA\n\n");
-   printf("No.: \tValor:\tProporci%con\n",162);
-
-}
-void MostrarElementoHistograma(int secuencia, float valor, float proporcion)
-{
-   int cantidadCaracteres = (int)proporcion;
-   printf("%d\t%.2f\t",secuencia, valor);
-
-   while(cantidadCaracteres-- >0)
-   {
-      printf("%c",219);
-   }
-
-   printf(" %2.f%%\n",proporcion);
-
-}
-void MostrarPieHistograma(int cantidad, float total)
-{
-   printf("Se han mostrado %d elementos. Total: %.2f",cantidad ,total);
 }
