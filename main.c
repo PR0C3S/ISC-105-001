dae56edd170719f4d12ddc930de4236e13ca41db
PR0C3S <50205406+PR0C3S@users.noreply.github.com>
2019-07-08 21:06:52 -0300

    Clase 2 - Pila dinamica/punteros

--- main.c
+++ main.c
@@ -1,19 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "versionPuntero.h"
-#include "versionMatriz.h"
+#include "Pilaestatica.h"
 
+void MostrarEncabezadoHistograma();
+void MostrarElementoHistograma(int, float, float);
+void MostrarPieHistograma(int, float);
 
-int main()
-{
+int main(){
+   int cantidad=0;
+   float nuevoValor, total=0;
+    Pila *miPila;
+    miPila= IniciarPila(miPila);
 
-    int filaReina, columnaReina;
+    do
+    {
+       printf("\nDigite un valor: (-1 para terminar): ");
+       scanf("%f",&nuevoValor);
+       if(nuevoValor >= 0)
+       {
+            total+=nuevoValor;
+          //  cantidad++;
+         Push(miPila,nuevoValor);
+       }
+    }while(nuevoValor >=0);
+    // o calcular la cantidad de esta forma cantidad= Size(miPila);
+    MostrarEncabezadoHistograma();
 
-    printf("Digite la posicion inicial de la reina:");
-    scanf("%d %d", &filaReina, &columnaReina);
+    while(!isEmpty(miPila))
+    {
+       cantidad++;
+       nuevoValor= Pop(miPila);
+       MostrarElementoHistograma(cantidad, nuevoValor,nuevoValor/total*100);
+    }
 
-    //mainMatriz(filaReina, columnaReina);
-    mainPuntero(filaReina, columnaReina);
-
+    MostrarPieHistograma(cantidad,total);
     return 0;
+}
+
+void MostrarEncabezadoHistograma()
+{
+   system("cls");
+   printf("HISTOGRAMA\n\n");
+   printf("No.: \tValor:\tProporci%con\n",162);
+
+}
+void MostrarElementoHistograma(int secuencia, float valor, float proporcion)
+{
+   int cantidadCaracteres = (int)proporcion;
+   printf("%d\t%.2f\t",secuencia, valor);
+
+   while(cantidadCaracteres-- >0)
+   {
+      printf("%c",219);
+   }
+
+   printf(" %2.f%%\n",proporcion);
+
+}
+void MostrarPieHistograma(int cantidad, float total)
+{
+   printf("Se han mostrado %d elementos. Total: %.2f",cantidad ,total);
 }
